e0f273de979c3b2b07d8737c4fee3ff62924d6de
Zack <2176651@cegepmontpetit.ca>
2022-10-06 15:49:43 -0400

    FCT Disposition du menu
    La disposition ("layout") du menu de l'application est terminé. Il affiche les deux choix suivants quand on clique dessus : "Supprimer toutes les questions" et "Supprimer tous les votes".

--- app/src/main/java/tanguay/votedroid/MainActivity.java
+++ app/src/main/java/tanguay/votedroid/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.Menu;
+
+import android.view.MenuItem;
+
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_supprimerQuestions) {
+            Toast.makeText(getApplicationContext(),"Supprimer toutes les questions",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.action_supprimerVotes) {
+            Toast.makeText(getApplicationContext(),"Supprimer tous les votes",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
