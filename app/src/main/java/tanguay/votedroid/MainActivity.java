9a73c4d46ccb699c784bcc50ab61d05fc5b8a151
Zack <2176651@cegepmontpetit.ca>
2022-10-20 15:44:36 -0400

    FCT Bouton Ajouter
    Le bouton ajouter est désormais fonctionnelle et redirige vers la page de création d'une question.

--- app/src/main/java/tanguay/votedroid/MainActivity.java
+++ app/src/main/java/tanguay/votedroid/MainActivity.java
@@ -2,20 +2,35 @@ package tanguay.votedroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.Menu;
 
 import android.view.MenuItem;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonAjouter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAjouter = (Button) findViewById(R.id.btnAjouter);
+        buttonAjouter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentAjouter = new Intent(MainActivity.this,CreationQuestion.class);
+                startActivity(intentAjouter);
+            }
+        });
     }
 
     @Override
